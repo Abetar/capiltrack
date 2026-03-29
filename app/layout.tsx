d52039665df39a4c0cf9bdaab75dbd261184471e
Abetar <alexisgomez_96@hotmail.com>
2026-03-28 18:29:09 -0600

    Adding metadata in capiltrack

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
 import { Inter } from "next/font/google"
 import React from "react"
 import "./globals.css"
+import { Metadata } from "next/dist/lib/metadata/types/metadata-interface";
 
 const inter = Inter({
   subsets: ["latin"],
 })
+
+export const metadata: Metadata = {
+  title: "CapilTrack - Gestión de pacientes y seguimiento clínico para clínicas capilares",
+  description: "CapilTrack es un software clínico diseñado para ayudar a las clínicas capilares a gestionar pacientes, realizar seguimientos clínicos y analizar resultados de manera eficiente. Con una interfaz intuitiva y herramientas avanzadas, CapilTrack facilita la organización de datos, el monitoreo de tratamientos y la generación de informes detallados para mejorar la atención al paciente y optimizar los procesos clínicos.",
+};
+  
 
 export default function RootLayout({
   children,
